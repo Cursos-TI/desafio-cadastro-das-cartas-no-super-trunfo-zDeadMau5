da09d41f8acae4e9e0422b1af4065d8b10e71a19
Guilherme Mateus <104778520+zDeadMau5@users.noreply.github.com>
2025-06-15 19:22:07 +0000

    Super Trunfo em C: desenvolvendo a lógica do jogo iniciante

--- super_trunfo.c
+++ super_trunfo.c
@@ -2,103 +2,122 @@
 #include <stdlib.h>
 
 int main() {
-    // Constante para definir o número de cartas, facilitando futuras modificações
-    const int NUM_CARTAS = 5;
+    // --- Declaração de Variáveis para a Carta 1 ---
+    char estado1[3];            // String para a sigla do estado (ex: "SP")
+    char codigoCarta1[5];       // String para o código (ex: "A01")
+    char nomeCidade1[50];       // String para o nome da cidade
+    unsigned long int populacao1; // Usando unsigned long int para populações grandes
+    float area1;
+    float pib1;                 // PIB em bilhões
+    int pontosTuristicos1;
+    float densidadePopulacional1;
+    float pibPerCapita1;
 
-    // --- Arrays para armazenar os dados de todas as cartas ---
-    char estadoCarta[NUM_CARTAS];
-    char codigoCarta[NUM_CARTAS][5];
-    char nomeCidade[NUM_CARTAS][50];
-    unsigned long int populacao[NUM_CARTAS];
-    float area[NUM_CARTAS];
-    float pib[NUM_CARTAS]; // Em bilhões
-    int pontosTuristicos[NUM_CARTAS];
-    float densidadePopulacional[NUM_CARTAS];
-    float pibPerCapita[NUM_CARTAS];
-    float superPoder[NUM_CARTAS];
+    // --- Declaração de Variáveis para a Carta 2 ---
+    char estado2[3];
+    char codigoCarta2[5];
+    char nomeCidade2[50];
+    unsigned long int populacao2;
+    float area2;
+    float pib2;
+    int pontosTuristicos2;
+    float densidadePopulacional2;
+    float pibPerCapita2;
 
-    // --- Leitura dos dados usando um laço 'for' ---
-    printf("--- Cadastro de %d Cartas Super Trunfo ---\n", NUM_CARTAS);
-    for (int i = 0; i < NUM_CARTAS; i++) {
-        printf("\n--- Inserindo dados da Carta %d ---\n", i + 1);
-        printf("Digite o estado (A-H): ");
-        scanf(" %c", &estadoCarta[i]);
-        printf("Digite o codigo (ex: A01): ");
-        scanf("%s", codigoCarta[i]);
-        printf("Digite o nome da cidade: ");
-        scanf(" %[^\n]s", nomeCidade[i]);
-        printf("Digite a populacao: ");
-        scanf("%lu", &populacao[i]); // %lu para unsigned long int
-        printf("Digite a area (km2): ");
-        scanf("%f", &area[i]);
-        printf("Digite o PIB (em bilhoes de reais): ");
-        scanf("%f", &pib[i]);
-        printf("Digite o numero de pontos turisticos: ");
-        scanf("%d", &pontosTuristicos[i]);
+    // --- BLOCO 1: ENTRADA DE DADOS ---
+    // Coleta de informações da primeira carta
+    printf("--- Cadastro da Primeira Carta ---\n");
+    printf("Digite a sigla do estado (ex: SP): ");
+    scanf("%s", estado1);
+    printf("Digite o codigo da carta (ex: A01): ");
+    scanf("%s", codigoCarta1);
+    printf("Digite o nome da cidade: ");
+    scanf(" %[^\n]s", nomeCidade1);
+    printf("Digite a populacao: ");
+    scanf("%lu", &populacao1); // Use %lu para unsigned long int
+    printf("Digite a area (km2): ");
+    scanf("%f", &area1);
+    printf("Digite o PIB (em bilhoes de reais): ");
+    scanf("%f", &pib1);
+    printf("Digite o numero de pontos turisticos: ");
+    scanf("%d", &pontosTuristicos1);
+    printf("\n");
+
+    // Coleta de informações da segunda carta
+    printf("--- Cadastro da Segunda Carta ---\n");
+    printf("Digite a sigla do estado (ex: RJ): ");
+    scanf("%s", estado2);
+    printf("Digite o codigo da carta (ex: B02): ");
+    scanf("%s", codigoCarta2);
+    printf("Digite o nome da cidade: ");
+    scanf(" %[^\n]s", nomeCidade2);
+    printf("Digite a populacao: ");
+    scanf("%lu", &populacao2);
+    printf("Digite a area (km2): ");
+    scanf("%f", &area2);
+    printf("Digite o PIB (em bilhoes de reais): ");
+    scanf("%f", &pib2);
+    printf("Digite o numero de pontos turisticos: ");
+    scanf("%d", &pontosTuristicos2);
+    printf("\n");
+
+    // --- BLOCO 2: CÁLCULOS DOS ATRIBUTOS DERIVADOS ---
+    // Cálculo para a Carta 1
+    densidadePopulacional1 = (float)populacao1 / area1;
+    pibPerCapita1 = (pib1 * 1000000000.0) / (float)populacao1;
+
+    // Cálculo para a Carta 2
+    densidadePopulacional2 = (float)populacao2 / area2;
+    pibPerCapita2 = (pib2 * 1000000000.0) / (float)populacao2;
+
+
+    // --- BLOCO 3: COMPARAÇÃO E EXIBIÇÃO DO RESULTADO ---
+    //
+    // NESTA SEÇÃO, O ATRIBUTO PARA COMPARAÇÃO É ESCOLHIDO.
+    // Para alterar, basta substituir as variáveis 'populacao1' e 'populacao2'
+    // pelas variáveis do atributo desejado (ex: area1 vs area2).
+    // Lembre-se de ajustar a regra de comparação se escolher Densidade Populacional.
+    //
+    printf("--------------------------------------------------\n");
+    printf("--- Comparacao de Cartas (Atributo: Populacao) ---\n");
+    printf("--------------------------------------------------\n\n");
+
+    // Exibe os valores das duas cartas para o atributo escolhido
+    printf("Carta 1 - %s (%s): %lu\n", nomeCidade1, estado1, populacao1);
+    printf("Carta 2 - %s (%s): %lu\n\n", nomeCidade2, estado2, populacao2);
+
+    // Lógica para determinar e anunciar o vencedor
+    if (populacao1 > populacao2) {
+        printf("Resultado: Carta 1 (%s) venceu!\n", nomeCidade1);
+    } else if (populacao2 > populacao1) {
+        printf("Resultado: Carta 2 (%s) venceu!\n", nomeCidade2);
+    } else {
+        printf("Resultado: Empate!\n");
     }
+    printf("--------------------------------------------------\n");
 
-    // --- Cálculos para todas as cartas usando um laço 'for' ---
-    for (int i = 0; i < NUM_CARTAS; i++) {
-        // Evita divisão por zero
-        if (area[i] > 0) {
-            densidadePopulacional[i] = (float)populacao[i] / area[i];
-        } else {
-            densidadePopulacional[i] = 0;
-        }
-        
-        if (populacao[i] > 0) {
-            pibPerCapita[i] = (pib[i] * 1000000000.0) / (float)populacao[i];
-        } else {
-            pibPerCapita[i] = 0;
-        }
 
-        superPoder[i] = (float)populacao[i] + area[i] + (pib[i] * 1000000000.0) + (float)pontosTuristicos[i] + pibPerCapita[i];
-        // Adiciona o inverso da densidade, se a densidade não for zero
-        if (densidadePopulacional[i] > 0) {
-             superPoder[i] += (1.0 / densidadePopulacional[i]);
-        }
+    /*
+    // EXEMPLO: Se quisesse comparar por DENSIDADE POPULACIONAL (menor vence)
+    // O bloco de comparação seria assim:
+    
+    printf("------------------------------------------------------------\n");
+    printf("--- Comparacao de Cartas (Atributo: Densidade Populacional) ---\n");
+    printf("------------------------------------------------------------\n\n");
+
+    printf("Carta 1 - %s (%s): %.2f hab/km2\n", nomeCidade1, estado1, densidadePopulacional1);
+    printf("Carta 2 - %s (%s): %.2f hab/km2\n\n", nomeCidade2, estado2, densidadePopulacional2);
+
+    // Regra invertida: o menor valor vence
+    if (densidadePopulacional1 < densidadePopulacional2) {
+        printf("Resultado: Carta 1 (%s) venceu!\n", nomeCidade1);
+    } else if (densidadePopulacional2 < densidadePopulacional1) {
+        printf("Resultado: Carta 2 (%s) venceu!\n", nomeCidade2);
+    } else {
+        printf("Resultado: Empate!\n");
     }
+    printf("------------------------------------------------------------\n");
+    */
 
-    // --- Determinar a carta vencedora para cada atributo ---
-    int idxVencedorPop = 0, idxVencedorArea = 0, idxVencedorPIB = 0;
-    int idxVencedorPontos = 0, idxVencedorDensidade = 0, idxVencedorPIBpc = 0, idxVencedorPoder = 0;
-
-    for (int i = 1; i < NUM_CARTAS; i++) {
-        // Maior valor vence
-        if (populacao[i] > populacao[idxVencedorPop]) idxVencedorPop = i;
-        if (area[i] > area[idxVencedorArea]) idxVencedorArea = i;
-        if (pib[i] > pib[idxVencedorPIB]) idxVencedorPIB = i;
-        if (pontosTuristicos[i] > pontosTuristicos[idxVencedorPontos]) idxVencedorPontos = i;
-        if (pibPerCapita[i] > pibPerCapita[idxVencedorPIBpc]) idxVencedorPIBpc = i;
-        if (superPoder[i] > superPoder[idxVencedorPoder]) idxVencedorPoder = i;
-
-        // Regra especial para densidade: Menor valor vence
-        if (densidadePopulacional[i] < densidadePopulacional[idxVencedorDensidade]) idxVencedorDensidade = i;
-    }
-
-    // --- Exibição dos Resultados Finais ---
-    printf("\n\n--- Resumo e Analise das %d Cartas ---\n", NUM_CARTAS);
-
-    for (int i = 0; i < NUM_CARTAS; i++) {
-        printf("\n--- Carta %d: %s ---\n", i + 1, nomeCidade[i]);
-        printf("Populacao: %lu\n", populacao[i]);
-        printf("Area: %.2f km2\n", area[i]);
-        printf("PIB: %.2f bilhoes\n", pib[i]);
-        printf("Pontos Turisticos: %d\n", pontosTuristicos[i]);
-        printf("Densidade Populacional: %.2f hab/km2\n", densidadePopulacional[i]);
-        printf("PIB per Capita: R$ %.2f\n", pibPerCapita[i]);
-        printf("SUPER PODER: %.2f\n", superPoder[i]);
-    }
-
-    // --- Exibição dos Vencedores por Categoria ---
-    printf("\n\n--- VENCEDORES POR CATEGORIA ---\n");
-    printf("Maior Populacao:       Carta %d (%s) com %lu habitantes\n", idxVencedorPop + 1, nomeCidade[idxVencedorPop], populacao[idxVencedorPop]);
-    printf("Maior Area:            Carta %d (%s) com %.2f km2\n", idxVencedorArea + 1, nomeCidade[idxVencedorArea], area[idxVencedorArea]);
-    printf("Maior PIB:             Carta %d (%s) com %.2f bilhoes\n", idxVencedorPIB + 1, nomeCidade[idxVencedorPIB], pib[idxVencedorPIB]);
-    printf("Mais Pontos Turisticos:Carta %d (%s) com %d pontos\n", idxVencedorPontos + 1, nomeCidade[idxVencedorPontos], pontosTuristicos[idxVencedorPontos]);
-    printf("Menor Densidade:       Carta %d (%s) com %.2f hab/km2\n", idxVencedorDensidade + 1, nomeCidade[idxVencedorDensidade], densidadePopulacional[idxVencedorDensidade]);
-    printf("Maior PIB per Capita:  Carta %d (%s) com R$ %.2f\n", idxVencedorPIBpc + 1, nomeCidade[idxVencedorPIBpc], pibPerCapita[idxVencedorPIBpc]);
-    printf("Maior SUPER PODER:     Carta %d (%s) com %.2f\n", idxVencedorPoder + 1, nomeCidade[idxVencedorPoder], superPoder[idxVencedorPoder]);
-
-    return 0;
+    return 0; // Indica que o programa terminou com sucesso
 }
